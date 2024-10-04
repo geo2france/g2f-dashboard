37e38e6106a1175298df47080249a3d173779901
Jean-Baptiste DESBAS <jean-baptiste.desbas@hautsdefrance.fr>
2024-10-04 18:13:41 +0200

    jest : mocks CSS

--- jest.config.js
+++ jest.config.js
@@ -3,12 +3,12 @@ module.exports = {
   testEnvironment: 'jsdom',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest'  },
+    '^.+\\.(ts|tsx)$': 'ts-jest',  },
   //setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
   testMatch: ['**/?(*.)+(test).(ts|tsx)'],
   moduleNameMapper: {
     '\\.svg\\?react$': '<rootDir>/__mocks__/svgReact.tsx', // Mock pour le composant SVG
     '\\.svg\\?url$': '<rootDir>/__mocks__/svgUrl.tsx',     // Mock pour l'URL de l'image SVG
-
+    "\\.(css|less|sass|scss)$": "<rootDir>/__mocks__/styleMock.tsx", // Mock por les CSS
   },
 };
